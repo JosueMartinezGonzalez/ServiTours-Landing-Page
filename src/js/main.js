fb475d74638ef7098268c4226efe88624fb56cc6
Josué Martinez <josuearmandomartinez@gmail.com>
2022-03-08 18:14:48 -0500

    Se creo el validador del formulario al igual que la conexión Json con Formspree y comprovación exitosa

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
+// Vars
+const $form = document.querySelector('#form');
+const $nameUser = document.querySelector('#name');
+const $emailUser = document.querySelector('#email');
+const $phoneUser = document.querySelector('#phone');
+const $messageUser = document.querySelector('#message');
+
+
+
+// Functions
+function carouselAnimation() {
+  document.addEventListener('DOMContentLoaded', () => {
     const elementosCarousel = document.querySelectorAll('.carousel');
     M.Carousel.init(elementosCarousel, {
       duration: 150,
@@ -10,3 +21,49 @@ document.addEventListener('DOMContentLoaded', () => {
       noWrap: false
     });
   });
+}
+
+
+
+async function handleSubmit(event) {
+  if($nameUser.value === null || $nameUser.value === '') {
+    alert('Ingrese un nombre valido')
+  }
+  if($emailUser.value === null || $emailUser.value === '') {
+    alert('Ingrese un email valido')
+  }
+  if($phoneUser.value === null || $phoneUser.value === '' || $phoneUser.value <= 3000000000) {
+    alert('Ingrese un numero valido en Colombia')
+  }
+  if($messageUser.value === null || $messageUser.value === '') {
+    alert('Ingrese un mensaje expresando su interés')
+  }
+  
+  event.preventDefault();
+  const form = new FormData(this)
+  const response = await fetch(this.action, {
+    method: this.method,
+    body: form,
+    headers: {
+      'Accept': 'application/json'
+    }
+  })
+  if(response.ok) {
+    this.reset()
+    alert('Gracias por contactarnos, te escribiremos pronto')
+  }
+  
+}
+
+
+
+
+
+
+
+
+
+
+// Execute Functions
+carouselAnimation();
+$form.addEventListener('submit', handleSubmit)
